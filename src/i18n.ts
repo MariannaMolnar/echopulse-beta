941de150abdb31b9e9bc1c38a3496d6d06e39633
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2025-03-18 18:03:36 +0100

    implementing i18next configuration file

---
+++ src/i18n.ts
@@ -0,0 +1,24 @@
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import en from "./i18n/en.json";
+import hu from "./i18n/hu.json";
+
+export const resources = {
+  en: {
+    translation: en,
+  },
+  hu: {
+    translation: hu,
+  },
+} as const;
+
+i18next.use(initReactI18next).init({
+  resources,
+  lng: "en",
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18next;
