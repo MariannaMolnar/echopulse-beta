8eb64b9ca32867ddff4dd123dfc80c8d3250f69a
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2025-03-20 16:38:37 +0100

    adding introduction section to about us page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,17 @@
-
+import { useTranslation } from "react-i18next";
 
 function About() {
+  const { t } = useTranslation();
+
   return (
-    <div>About</div>
-  )
+    <div className="w-full flex flex-col items-center gap-16 py-16">
+        {/* Hero Section 1: Introduction */}
+        <section className="hero-section">
+            <h2 className="hero-title">{t("about.title")}</h2>
+            <p className="hero-text">{t("about.intro")}</p>
+        </section>
+    </div>
+  );
 }
 
-export default About
+export default About;
