0f6d1bf471bc94563ed26331b4c13c4426bde5db
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2025-03-23 17:28:36 +0100

    adding fade-out div to layout below navbar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,12 @@
 import Navbar from "./Navbar";
 import { Outlet } from "react-router";
 
-//type Props = {};
-
 function Layout() {
   return (
     <div className="bg-background-1 bg-fixed min-h-screen w-full flex flex-col">
       <Navbar />
-      <div className="flex-1 pt-[4rem]">
+      <div className="flex-1 pt-[2rem] relative overflow-hidden z-0">
+        <div className="fixed top-0 left-0 w-full h-22 fade-out z-30"></div>
         <Outlet />
       </div>
     </div>
