1a83860685387d7f04afedba80470364f5792b12
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2025-03-26 21:07:37 +0100

    adding more classnames and changing some icons to different style

--- src/components/ArtistCard.tsx
+++ src/components/ArtistCard.tsx
@@ -1,6 +1,7 @@
 import { Artist } from "../artists/data/artists";
-import { FaFacebookF, FaSpotify, FaSoundcloud, FaYoutube, FaGlobe } from "react-icons/fa";
+import { FaFacebook, FaSpotify, FaYoutube, FaGlobe } from "react-icons/fa";
 import { BsGlobe2 } from "react-icons/bs";
+import { PiSoundcloudLogoFill } from "react-icons/pi";
 
 type Props = {
   artist: Artist;
@@ -16,36 +17,36 @@ function ArtistCard({ artist }: Props) {
       {/* Artist info */}
       <div className="artist-info-container">
         <div className="artist-info">
-          <p className="artist-name">{artist.name}</p>
+          <p className="artist-name">{artist.name.toUpperCase()}</p>
           <p className="artist-country">{artist.locationCountry}</p>
         </div>
         {/* Genre tags - to be implemented */}
       </div>
       {/* Artist social links icons */}
-      <div className="artist-social-links">
+      <div className="artist-social-links-contanier">
         {artist.socialLinks.soundCloud && (
-            <a href={artist.socialLinks.soundCloud} target="_blank" rel="noopener noreferrer">
-                <FaSoundcloud />
+            <a href={artist.socialLinks.soundCloud} target="_blank" rel="noopener noreferrer" className="artist-social-link">
+                <PiSoundcloudLogoFill className="artist-social-icon"/>
             </a>
         )}
         {artist.socialLinks.spotify && (
-            <a href={artist.socialLinks.spotify} target="_blank" rel="noopener noreferrer">
-                <FaSpotify />
+            <a href={artist.socialLinks.spotify} target="_blank" rel="noopener noreferrer" className="artist-social-link">
+                <FaSpotify className="artist-social-icon"/>
             </a>
         )}
         {artist.socialLinks.youtube && (
-            <a href={artist.socialLinks.youtube} target="_blank" rel="noopener noreferrer">
-                <FaYoutube />
+            <a href={artist.socialLinks.youtube} target="_blank" rel="noopener noreferrer" className="artist-social-link">
+                <FaYoutube className="artist-social-icon"/>
             </a>
             )}
         {artist.socialLinks.facebook && (
-            <a href={artist.socialLinks.facebook} target="_blank" rel="noopener noreferrer">
-                <FaFacebookF />
+            <a href={artist.socialLinks.facebook} target="_blank" rel="noopener noreferrer" className="artist-social-link">
+                <FaFacebook className="artist-social-icon"/>
             </a>
         )}
         {artist.socialLinks.website && (
-            <a href={artist.socialLinks.website} target="_blank" rel="noopener noreferrer">
-                <BsGlobe2 />
+            <a href={artist.socialLinks.website} target="_blank" rel="noopener noreferrer" className="artist-social-link">
+                <BsGlobe2 className="artist-social-icon"/>
             </a>
         )}
       </div>
