c0a315566265dbdd3ac86033099c9747c8c72ffa
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2025-03-26 11:10:46 +0100

    adding all artists page title and tailwind css classname

--- src/pages/AllArtists.tsx
+++ src/pages/AllArtists.tsx
@@ -2,8 +2,16 @@ import { useTranslation } from "react-i18next";
 
 function AllArtists() {
   const { t } = useTranslation();
-  
-  return <div>AllArtists</div>;
+
+  return (
+    <div className="isolate w-full flex flex-col items-center px-6 py-24 sm:py-32 lg:px-8">
+      <div className="w-full bg-black/60 text-gray-300 py-20 px-6 rounded-xl shadow-lg">
+        <div className="mx-auto max-w-2xl text-center">
+            <h2 className="allArtists-title">{t("allArtists.title")}</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AllArtists;
