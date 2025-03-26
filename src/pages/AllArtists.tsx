0d76c978e81b05a9e2812a9401ba7c3495b8d9a6
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2025-03-26 11:32:29 +0100

    adding comments to all artists component

--- src/pages/AllArtists.tsx
+++ src/pages/AllArtists.tsx
@@ -6,9 +6,13 @@ function AllArtists() {
   return (
     <div className="isolate w-full flex flex-col items-center px-6 py-24 sm:py-32 lg:px-8">
       <div className="w-full bg-black/60 text-gray-300 py-20 px-6 rounded-xl shadow-lg">
+        {/* Page title */}
         <div className="mx-auto max-w-2xl text-center">
-            <h2 className="allArtists-title">{t("allArtists.title")}</h2>
+          <h2 className="allArtists-title">{t("allArtists.title")}</h2>
         </div>
+
+        {/* Filter and search - to be implemented */}
+        
       </div>
     </div>
   );
