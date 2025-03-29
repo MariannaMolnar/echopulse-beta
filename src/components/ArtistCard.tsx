88ab7e627e6c0ab43d8cabae1d822a17a6b0db4f
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2025-03-29 20:42:13 +0100

    refactoring artistcard to reference countryname in line with new structure

--- src/components/ArtistCard.tsx
+++ src/components/ArtistCard.tsx
@@ -9,7 +9,7 @@ type Props = {
 };
 
 function ArtistCard({ artist }: Props) {
-    const { language } = useLanguage();
+  const { language } = useLanguage();
 
   return (
     <div className="artist-card group">
@@ -22,36 +22,63 @@ function ArtistCard({ artist }: Props) {
       <div className="artist-info-container">
         <div className="artist-info">
           <p className="artist-name">{artist.name.toUpperCase()}</p>
-          <p className="artist-country">{artist.locationCountry[language as keyof typeof artist.locationCountry]}</p>
+          <p className="artist-country">
+            {artist.country.name[language as keyof typeof artist.country.name]}
+          </p>
         </div>
         {/* Genre tags - to be implemented */}
       </div>
       {/* Artist social links icons */}
       <div className="artist-social-links-contanier">
         {artist.socialLinks.soundCloud && (
-            <a href={artist.socialLinks.soundCloud} target="_blank" rel="noopener noreferrer" className="artist-social-link">
-                <PiSoundcloudLogoFill className="artist-social-icon"/>
-            </a>
+          <a
+            href={artist.socialLinks.soundCloud}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="artist-social-link"
+          >
+            <PiSoundcloudLogoFill className="artist-social-icon" />
+          </a>
         )}
         {artist.socialLinks.spotify && (
-            <a href={artist.socialLinks.spotify} target="_blank" rel="noopener noreferrer" className="artist-social-link">
-                <FaSpotify className="artist-social-icon"/>
-            </a>
+          <a
+            href={artist.socialLinks.spotify}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="artist-social-link"
+          >
+            <FaSpotify className="artist-social-icon" />
+          </a>
         )}
         {artist.socialLinks.youtube && (
-            <a href={artist.socialLinks.youtube} target="_blank" rel="noopener noreferrer" className="artist-social-link">
-                <FaYoutube className="artist-social-icon"/>
-            </a>
-            )}
+          <a
+            href={artist.socialLinks.youtube}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="artist-social-link"
+          >
+            <FaYoutube className="artist-social-icon" />
+          </a>
+        )}
         {artist.socialLinks.facebook && (
-            <a href={artist.socialLinks.facebook} target="_blank" rel="noopener noreferrer" className="artist-social-link">
-                <FaFacebook className="artist-social-icon"/>
-            </a>
+          <a
+            href={artist.socialLinks.facebook}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="artist-social-link"
+          >
+            <FaFacebook className="artist-social-icon" />
+          </a>
         )}
         {artist.socialLinks.website && (
-            <a href={artist.socialLinks.website} target="_blank" rel="noopener noreferrer" className="artist-social-link">
-                <BsGlobe2 className="artist-social-icon"/>
-            </a>
+          <a
+            href={artist.socialLinks.website}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="artist-social-link"
+          >
+            <BsGlobe2 className="artist-social-icon" />
+          </a>
         )}
       </div>
     </div>
