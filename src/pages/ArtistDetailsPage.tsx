06ff8e981382aa77888bf6c76aa7a8b2d8d3f4cc
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2025-04-13 21:11:41 +0200

    removing unused code

--- src/pages/ArtistDetailsPage.tsx
+++ src/pages/ArtistDetailsPage.tsx
@@ -1,12 +1,7 @@
-//import { Artist } from '../data/artists'
 import { artists } from "../data/artists";
 import { useParams } from "react-router";
 import { Artist } from "../data/artists";
 import ProfileHeader from "../components/molecules/ProfileHeader";
-
-/*type ArtistDetailsProps = {
-    artist: Artist;
-}*/
 
 function getArtist(artistName: string | undefined): Artist | null {
   if (!artistName) return null;
@@ -14,9 +9,10 @@ function getArtist(artistName: string | undefined): Artist | null {
   return artistInDb;
 }
 
-function ArtistDetailsPage(/*{artist}: ArtistDetailsProps*/) {
+function ArtistDetailsPage() {
   const { artistName } = useParams();
   const artist = getArtist(artistName);
+
   return (
     <div className="isolate w-full flex flex-col items-center px-4 md:px-6 py-24 sm:py-32 lg:px-8">
       <div className="w-full bg-black/60 text-gray-300 py-20 px-4 md:px-6 rounded-xl shadow-lg">
