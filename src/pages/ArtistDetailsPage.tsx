aff4695040c7b643931b2b835c6d0c44cbb8716b
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2025-04-13 18:55:04 +0200

    implementing artist retrieval to give as a prop

--- src/pages/ArtistDetailsPage.tsx
+++ src/pages/ArtistDetailsPage.tsx
@@ -1,17 +1,26 @@
 //import { Artist } from '../data/artists'
-
+import { artists } from "../data/artists";
 import { useParams } from "react-router";
+import { Artist } from "../data/artists";
+import ProfileHeader from "../components/molecules/ProfileHeader";
 
 /*type ArtistDetailsProps = {
     artist: Artist;
 }*/
 
+function getArtist(artistName: string | undefined): Artist | null {
+  if (!artistName) return null;
+  const artistInDb = artists.filter((artist) => artist.name === artistName)[0];
+  return artistInDb;
+}
+
 function ArtistDetailsPage(/*{artist}: ArtistDetailsProps*/) {
   const { artistName } = useParams();
+  const artist = getArtist(artistName);
   return (
     <div className="isolate w-full flex flex-col items-center px-4 md:px-6 py-24 sm:py-32 lg:px-8">
       <div className="w-full bg-black/60 text-gray-300 py-20 px-4 md:px-6 rounded-xl shadow-lg">
-        <div className="text-white mt-52">{artistName}</div>
+        {artist && <ProfileHeader artist={artist} />}
       </div>
     </div>
   );
