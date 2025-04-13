a908689c26712b81d7c262295c4a07ea92854c4b
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2025-04-13 17:30:08 +0200

    adding some basic style to hte ArtistDetailsPage

--- src/pages/ArtistDetailsPage.tsx
+++ src/pages/ArtistDetailsPage.tsx
@@ -1,16 +1,20 @@
 //import { Artist } from '../data/artists'
 
-import { useParams } from "react-router"
+import { useParams } from "react-router";
 
 /*type ArtistDetailsProps = {
     artist: Artist;
 }*/
 
 function ArtistDetailsPage(/*{artist}: ArtistDetailsProps*/) {
-    const {artistName} = useParams();
+  const { artistName } = useParams();
   return (
-    <div className="text-white mt-52">{artistName}</div>
-  )
+    <div className="isolate w-full flex flex-col items-center px-4 md:px-6 py-24 sm:py-32 lg:px-8">
+      <div className="w-full bg-black/60 text-gray-300 py-20 px-4 md:px-6 rounded-xl shadow-lg">
+        <div className="text-white mt-52">{artistName}</div>
+      </div>
+    </div>
+  );
 }
 
-export default ArtistDetailsPage
+export default ArtistDetailsPage;
