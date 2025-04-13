2217aba63a4f7cb7c41dac04661f800bd2823295
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2025-04-13 21:27:12 +0200

    structuring rest of the content

--- src/pages/ArtistDetailsPage.tsx
+++ src/pages/ArtistDetailsPage.tsx
@@ -16,7 +16,17 @@ function ArtistDetailsPage() {
   return (
     <div className="isolate w-full flex flex-col items-center px-4 md:px-6 py-24 sm:py-32 lg:px-8">
       <div className="w-full bg-black/60 text-gray-300 py-20 px-4 md:px-6 rounded-xl shadow-lg">
+        {/* Header section */}
         {artist && <ProfileHeader artist={artist} />}
+        {/* Details section */}
+        <div className="grid grid-cols-[75%_25%]">
+          {/* Left column - About section */}
+          <div>Item 1 (75%)</div>
+          {/* Right column section */}
+          <div>Item 2 (25%)</div>
+          {/* Socials section */}
+          {/* Next Events section */}
+        </div>
       </div>
     </div>
   );
