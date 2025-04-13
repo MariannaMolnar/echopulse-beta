0de187375182e83b3507d831f13832b2d5e4d4eb
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2025-04-13 21:01:48 +0200

    implementing ProfileHeader

--- src/components/molecules/ProfileHeader.tsx
+++ src/components/molecules/ProfileHeader.tsx
@@ -1,3 +1,4 @@
+import ReactCountryFlag from "react-country-flag";
 import { useLanguage } from "../../context/LanguageContext";
 import { Artist } from "../../data/artists";
 import ArtistProfileAvatar from "../atoms/ArtistProfileAvatar";
@@ -8,9 +9,36 @@ type HeaderProps = {
 
 function ProfileHeader({ artist }: HeaderProps) {
   const { language } = useLanguage();
+  const artistCountry =
+    artist.country.name[language as keyof typeof artist.country.name];
+
   return (
-    <div className="w-full bg-black/60 text-gray-300 py-20 px-4 md:px-6 rounded-xl shadow-lg">
+    <div className="w-full bg-black/60 text-gray-300 py-4 md:py-6 px-4 md:px-6 rounded-xl shadow-lg flex items-center gap-4 md:gap-6">
+      {/* Avatar */}
       <ArtistProfileAvatar artist={artist} />
+
+      {/* Info Block: Stacks Name and Country vertically */}
+      <div className="flex flex-col">
+        {/* Artist Name */}
+        <h2 className="text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl font-bold text-white mb-1">
+          {artist.name}
+        </h2>
+
+        {/* Country Info: Uses flex row internally */}
+        <div className="flex items-baseline gap-2  text-gray-400">
+          {/* Flag Container */}
+          <div className="inline-flex items-center justify-center w-5 h-4 shrink-0">
+            <ReactCountryFlag
+              countryCode={artist.country.code}
+              svg
+              style={{ width: "2em", height: "1em" }}
+              className="rounded-xs"
+            />
+          </div>
+          {/* Country Name Span */}
+          <span className="min-w-0 truncate text-xs md:text-base">{artistCountry}</span>
+        </div>
+      </div>
     </div>
   );
 }
