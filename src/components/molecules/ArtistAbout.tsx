31720f0c4dc61bc7359d6789013cf15b55203fd0
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2025-04-14 16:03:24 +0200

    using artist.description for the text

--- src/components/molecules/ArtistAbout.tsx
+++ src/components/molecules/ArtistAbout.tsx
@@ -11,16 +11,15 @@ function ArtistAbout({ artist }: ArtistAboutProps) {
   const { language } = useLanguage();
   const aboutText =
     artist.description[language as keyof typeof artist.description];
-  const fillingText =
-    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi asperiores tenetur quae maiores voluptate doloribus, veritatis impedit, aliquam repudiandae natus iure at reiciendis labore animi laborum ad, explicabo ut beatae.";
-  const testText = `${fillingText}\n\n${fillingText}${fillingText}\n\n${fillingText}`;
 
   return (
     <div className="w-full bg-black/60 text-gray-300 rounded-xl shadow-lg py-4 mt-4 md:py-6 px-4 md:px-6">
       <h2 className="text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl font-bold text-white pb-6">
         {t("artistDetails.sectionTitles.about")}
       </h2>
-      <p className="whitespace-pre-line text-justify text-base md:text-md lg:text-lg xl:text-xl 2xl:text-2xl">{testText}</p>
+      <p className="whitespace-pre-line text-justify text-base md:text-md lg:text-lg xl:text-xl 2xl:text-2xl">
+        {aboutText}
+      </p>
     </div>
   );
 }
