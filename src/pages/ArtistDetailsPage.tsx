ad5a506efa836595bcd6891537a817436d5ad8f9
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2025-04-14 20:33:25 +0200

    adding different responsive layout

--- src/pages/ArtistDetailsPage.tsx
+++ src/pages/ArtistDetailsPage.tsx
@@ -21,13 +21,17 @@ function ArtistDetailsPage() {
           {/* Header section */}
           <ProfileHeader artist={artist} />
           {/* Details section */}
-          <div className="grid grid-cols-[75%_25%] gap-4 md:gap-5 xl:gap-6 pr-4 md:pr-6">
+          <div className="flex flex-col gap-6 lg:grid lg:grid-cols-[75%_25%] lg:gap-4 xl:gap-5 lg:pr-6 mt-6 md:mt-8">
             {/* Left column - About section */}
-            <ArtistAbout artist={artist} />
+            <div className="order-2 lg:order-none">
+              <ArtistAbout artist={artist} />
+            </div>
             {/* Right column section */}
-            <div className="w-full bg-black/60 text-gray-300 rounded-xl shadow-lg py-4 mt-4 md:py-6">Item 2 (25%)</div>
-            {/* Socials section */}
-            {/* Next Events section */}
+            <div className="order-1 lg:order-none w-full bg-black/60 text-gray-300 rounded-xl shadow-lg py-4 mt-4 md:py-6">
+              Item 2 (25%)
+              {/* Socials section */}
+              {/* Next Events section */}
+            </div>
           </div>
         </div>
       </div>
