f64c5c8162d4033a5a295a7309414eb6baf15ef4
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2025-04-15 17:28:15 +0200

    adding info middle section to artisteventcard

--- src/components/atoms/ArtistNextEventCard.tsx
+++ src/components/atoms/ArtistNextEventCard.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from "react-i18next";
 import { useLanguage } from "../../context/LanguageContext";
 import { Event } from "../../data/events";
 import DateBlockResponsive from "./DateBlockResponsive";
+import ReactCountryFlag from "react-country-flag";
 
 type NextEventProps = {
   artistEvent: Event;
@@ -32,7 +33,29 @@ function ArtistNextEventCard({ artistEvent }: NextEventProps) {
         />
       </div>
 
-
+      {/* --- Middle Section: Info --- */}
+      <div className="flex-1 flex flex-col min-w-0">
+        <p className="font-bold text-base md:text-lg text-white truncate mb-1">
+          {artistEvent.name}
+        </p>
+        {/* Location Info */}
+        <div className="flex items-baseline gap-2 text-xs md:text-sm text-gray-400">
+          {/* Flag Container */}
+          <div className="relative top-0.5 w-5 h-4 inline-flex items-center justify-center shrink-0 rounded-xs overflow-hidden">
+            <ReactCountryFlag
+              countryCode={artistEvent.address.country.code}
+              svg
+              style={{ width: "2em", height: "1em" }}
+              className="rounded-xs"
+            />
+          </div>
+          {/* Location Text */}
+          <span className="inline min-w-0">
+            <span className="truncate">{eventCity}</span>
+            <span className="inline">{`, ${eventCountry}`}</span>
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
