84bf352b59206bf2f07c2459b6d5acc815f9ee24
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2025-04-15 16:36:05 +0200

    implementing ArtistNextEvents component

--- src/components/molecules/ArtistNextEvents.tsx
+++ src/components/molecules/ArtistNextEvents.tsx
@@ -1,13 +1,24 @@
-import { Event } from '../../data/events'
+import { useTranslation } from "react-i18next";
+import { Event } from "../../data/events";
+import ArtistNextEventCard from "../atoms/ArtistNextEventCard";
 
-type Props = {
-    artistNextEvents: Event[] | null
-}
+type NextEventsProps = {
+  artistNextEvents: Event[] | null;
+};
 
-function ArtistNextEvents({artistNextEvents}: Props) {
+function ArtistNextEvents({ artistNextEvents }: NextEventsProps) {
+  const { t } = useTranslation();
+
   return (
-    <div className="flex flex-col w-full gap-3 px-3 py-3 bg-black/50 text-white rounded-xl flex-shrink-0">ArtistNextEvents</div>
-  )
+    <div className="flex flex-col w-full gap-3 px-6 2xl:px-8 py-6 bg-black/50 text-white rounded-xl flex-shrink-0">
+      <h2 className="text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl font-bold text-white pb-6">
+        {t("artistDetails.sectionTitles.nextEvents")}
+      </h2>
+      {artistNextEvents?.map((artistEvent) => (
+        <ArtistNextEventCard key={artistEvent.id} artistEvent={artistEvent} />
+      ))}
+    </div>
+  );
 }
 
-export default ArtistNextEvents
+export default ArtistNextEvents;
