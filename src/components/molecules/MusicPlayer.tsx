aa57804ba10118c980cce0e9709d1b9615fc4fa9
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2025-04-16 21:02:47 +0200

    adding section title to musicplayer

--- src/components/molecules/MusicPlayer.tsx
+++ src/components/molecules/MusicPlayer.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from "react-i18next";
 import { Artist } from "../../data/artists";
 import parse from "html-react-parser";
 
@@ -8,8 +9,13 @@ type MusicPlayerProps = {
 function MusicPlayer({ artist }: MusicPlayerProps) {
   const artistSCEmbedding = artist.embedding.soundcloud;
   const artistSpotifyEmbedding = artist.embedding.spotify;
+  const {t} = useTranslation();
+
   return (
     <div className="flex flex-col w-full gap-3 px-6 2xl:px-8 py-6 bg-black/50 text-white rounded-xl flex-shrink-0">
+      <h2 className="text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl font-bold text-white pb-6">
+        {t("artistDetails.sectionTitles.mediaplayer")}
+      </h2>
       {artistSCEmbedding && parse(artistSCEmbedding)}
       {artistSpotifyEmbedding && parse(artistSpotifyEmbedding)}
     </div>
