cdaac42ad219eccc9ddc89326b84c95a68f6a129
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2025-04-21 08:44:37 +0200

    refactoring artistcard css from index.css to classname utilities inside component

--- src/components/molecules/ArtistCard.tsx
+++ src/components/molecules/ArtistCard.tsx
@@ -1,5 +1,5 @@
 import { Artist } from "../../data/artists";
-import { FaFacebook, FaSpotify, FaYoutube, FaGlobe } from "react-icons/fa";
+import { FaFacebook, FaSpotify, FaYoutube } from "react-icons/fa";
 import { BsGlobe2 } from "react-icons/bs";
 import { PiSoundcloudLogoFill } from "react-icons/pi";
 import { useLanguage } from "../../context/LanguageContext";
@@ -15,18 +15,18 @@ function ArtistCard({ artist }: Props) {
   const linkTo = `/artists/${artist.name}`;
 
   return (
-    <div className="artist-card group">
+    <div className="flex flex-col bg-[#f0edf2] overflow-hidden rounded-md text-black border-[1px] border-[#140b22] group">
       {/* Artist image */}
-      <div className="artist-image-container">
+      <div className="relative overflow-hidden w-full flex-shrink-0">
         <Link to={linkTo}>
-          <img src={artist.image} className="artist-image" />
-          <p className="artist-name-overlay">{artist.name.toUpperCase()}</p>
+          <img src={artist.image} className="w-full h-auto block transition-transform duration-300 ease-in-out group-hover:scale-105 group-hover:brightness-95 transition-all brightness-75 sm:brightness-100" />
+          <p className="block sm:hidden absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white font-semibold text-[4.5vw] uppercase [text-shadow:0px_0px_2px_black] w-full p-[5px] text-center pointer-events-none">{artist.name.toUpperCase()}</p>
         </Link>
       </div>
       {/* Artist info */}
-      <div className="artist-info-container">
-        <div className="artist-info">
-          <p className="artist-name">{artist.name.toUpperCase()}</p>
+      <div className="hidden sm:flex w-full justify-between items-start p-3 flex-1 bg-black/50 text-white flex-shrink-0">
+        <div>
+          <p className="text-lg font-bold mb-2">{artist.name.toUpperCase()}</p>
           {/* Container for Flag + Country Name */}
           <div className="flex items-center gap-2">
             <div className="inline-flex items-center justify-center w-5 h-4 shrink-0">
@@ -37,7 +37,7 @@ function ArtistCard({ artist }: Props) {
                 className="rounded-xs"
               />
             </div>
-            <p className="artist-country">
+            <p className="text-base">
               {
                 artist.country.name[
                   language as keyof typeof artist.country.name
@@ -49,15 +49,15 @@ function ArtistCard({ artist }: Props) {
         {/* Genre tags - to be implemented */}
       </div>
       {/* Artist social links icons */}
-      <div className="artist-social-links-contanier">
+      <div className="hidden sm:flex mt-auto w-full justify-end gap-3 px-3 pb-3 bg-black/50 text-white flex-shrink-0">
         {artist.socialLinks.soundCloud && (
           <a
             href={artist.socialLinks.soundCloud}
             target="_blank"
             rel="noopener noreferrer"
-            className="artist-social-link"
+            className="inline-flex items-center justify-center text-center text-lg"
           >
-            <PiSoundcloudLogoFill className="artist-social-icon" />
+            <PiSoundcloudLogoFill className="text-[1.3em] brightness-75 hover:brightness-100" />
           </a>
         )}
         {artist.socialLinks.spotify && (
@@ -65,9 +65,9 @@ function ArtistCard({ artist }: Props) {
             href={artist.socialLinks.spotify}
             target="_blank"
             rel="noopener noreferrer"
-            className="artist-social-link"
+            className="inline-flex items-center justify-center text-center text-lg"
           >
-            <FaSpotify className="artist-social-icon" />
+            <FaSpotify className="text-[1.3em] brightness-75 hover:brightness-100" />
           </a>
         )}
         {artist.socialLinks.youtube && (
@@ -75,9 +75,9 @@ function ArtistCard({ artist }: Props) {
             href={artist.socialLinks.youtube}
             target="_blank"
             rel="noopener noreferrer"
-            className="artist-social-link"
+            className="inline-flex items-center justify-center text-center text-lg"
           >
-            <FaYoutube className="artist-social-icon" />
+            <FaYoutube className="text-[1.3em] brightness-75 hover:brightness-100" />
           </a>
         )}
         {artist.socialLinks.facebook && (
@@ -85,9 +85,9 @@ function ArtistCard({ artist }: Props) {
             href={artist.socialLinks.facebook}
             target="_blank"
             rel="noopener noreferrer"
-            className="artist-social-link"
+            className="inline-flex items-center justify-center text-center text-lg"
           >
-            <FaFacebook className="artist-social-icon" />
+            <FaFacebook className="text-[1.3em] brightness-75 hover:brightness-100" />
           </a>
         )}
         {artist.socialLinks.website && (
@@ -95,9 +95,9 @@ function ArtistCard({ artist }: Props) {
             href={artist.socialLinks.website}
             target="_blank"
             rel="noopener noreferrer"
-            className="artist-social-link"
+            className="inline-flex items-center justify-center text-center text-lg"
           >
-            <BsGlobe2 className="artist-social-icon" />
+            <BsGlobe2 className="text-[1.3em] brightness-75 hover:brightness-100" />
           </a>
         )}
       </div>
