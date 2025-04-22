d55a31169d9161a3bb0ea4c4a9c95040d372eeeb
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2025-04-22 22:42:38 +0200

    changing navbar height and background to black gradient

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -33,7 +33,7 @@ function Navbar() {
   }
 
   return (
-    <nav className="bg-black shadow-md fixed top-0 left-0 w-full z-50">
+    <nav className="fade-out-2 fixed top-0 left-0 w-full z-50 h-[138px]">
       <div className="max-w-screen mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Left Section - Logo */}
