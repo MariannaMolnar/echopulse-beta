fb0cc9302f77b107549ba661f86741a2d88677a9
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2025-04-27 09:07:00 +0200

    adjusting carousel type definitions and array

--- src/data/carousel.ts
+++ src/data/carousel.ts
@@ -11,70 +11,76 @@ import sandImg from "../images/backdrops/sand.jpg";
 import warmWavesAndSplash from "../images/backdrops/warmWavesAndSplash.jpg";
 import wood from "../assets/wood.jpg";
 
-interface BaseSlide {
-    id: string;
-    imageUrl: string;
-    linkUrl?: string;
-    altText?: string; // Optional alt text
-  }
-  
-  interface EventSlideData {
-    date: Date;
-    title: LanguageSpecificString;
-    venueName: string;
-  }
-  
-  interface ReleaseSlideData {
-    releaseType: "album" | "track";
-    title: LanguageSpecificString;
-    artist: Artist;
-    releaseTitle: string;
-  }
-  
-  interface EventSlide extends BaseSlide {
-    type: "event";
-    data: EventSlideData;
-  }
-  
-  interface ReleaseSlide extends BaseSlide {
-    type: "release";
-    data: ReleaseSlideData;
-  }
-  
-  export type CarouselSlide = BaseSlide | EventSlide | ReleaseSlide;
-  
-  export const carousel: CarouselSlide[] = [
-    {
-      id: "1",
-      imageUrl: wood, //logo backdrop
-    },
-    {
-      id: "2",
-      imageUrl: redMelt,
-      altText: "Let it flow - Vödör bár",
-      type: "event",
-      data: {
-        date: new Date("2025-05-05"),
-        title: {
-          hu: "Hadd fojjon",
-          en: "Let it flow",
-        },
-        venueName: "Vödör bár",
+
+export interface BaseSlide {
+  id: string;
+  imageUrl: string;
+  linkUrl?: string;
+  altText?: string; // Optional alt text
+}
+
+export interface ImageSlide extends BaseSlide {
+  type: "image";
+}
+
+interface EventSlideData {
+  date: Date;
+  title: LanguageSpecificString;
+  venueName: string;
+}
+
+interface ReleaseSlideData {
+  releaseType: "album" | "track";
+  title: LanguageSpecificString;
+  artist: Artist;
+  releaseTitle: string;
+}
+
+export interface EventSlide extends BaseSlide {
+  type: "event";
+  data: EventSlideData;
+}
+
+export interface ReleaseSlide extends BaseSlide {
+  type: "release";
+  data: ReleaseSlideData;
+}
+
+export type CarouselSlide = ImageSlide | EventSlide | ReleaseSlide;
+
+export const carousel: CarouselSlide[] = [
+  {
+    id: "1",
+    type: "image",
+    imageUrl: wood, //logo backdrop
+  },
+  {
+    id: "2",
+    imageUrl: redMelt,
+    altText: "Let it flow - Vödör bár",
+    type: "event",
+    data: {
+      date: new Date("2025-05-05"),
+      title: {
+        hu: "Hadd fojjon",
+        en: "Let it flow",
       },
+      venueName: "Vödör bár",
     },
-    {
-      id: "3",
-      imageUrl: blackMoss,
-      altText: "Shadow Echo new release",
-      type: "release",
-      data: {
-        releaseType: "track",
-        title: {
-          hu: "Új megjelenés",
-          en: "New release",
-        },
-        artist: artists[0],
-        releaseTitle: "Shadow Echo - Confessions of the moss",
+  },
+  {
+    id: "3",
+    imageUrl: blackMoss,
+    altText: "Shadow Echo new release",
+    type: "release",
+    data: {
+      releaseType: "track",
+      title: {
+        hu: "Új megjelenés",
+        en: "New release",
       },
+      artist: artists[0],
+      releaseTitle: "Shadow Echo - Confessions of the moss",
     },
-  ];
+  },
+];
